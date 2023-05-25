01359f7dcfca9efd775f7a7bf8b2943d0005fbf6
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2023-05-25 00:34:44 -0400

    Create index.js

---
+++ proy/dist/entities/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Type = exports.Task = exports.User = void 0;
+var user_entity_1 = require("./user.entity");
+Object.defineProperty(exports, "User", { enumerable: true, get: function () { return user_entity_1.User; } });
+var task_entity_1 = require("./task.entity");
+Object.defineProperty(exports, "Task", { enumerable: true, get: function () { return task_entity_1.Task; } });
+var type_entity_1 = require("./type.entity");
+Object.defineProperty(exports, "Type", { enumerable: true, get: function () { return type_entity_1.Type; } });
+//# sourceMappingURL=index.js.map
