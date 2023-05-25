17ff50f1c2763704dbdccf5ff3669e7f82082fdf
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2023-05-25 00:39:52 -0400

    Create data.source.ts

---
+++ proy/src/config/data.source.ts
@@ -0,0 +1,25 @@
+import { ConfigModule } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import baseConfig from './base-config';
+import { User,Task, Type } from '../entities';
+
+ConfigModule.forRoot({
+    isGlobal: true,
+    load: [baseConfig],
+});
+
+export const DataSourceConfig: DataSourceOptions = {
+    type: process.env.TYPEORM_CONNECTION as any,
+    host: process.env.TYPEORM_HOST,
+    port: parseInt(process.env.TYPEORM_PORT, 10),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    entities: [User, Task, Type],
+    synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+    logging: process.env.TYPEORM_LOGGING === 'true',
+    migrations: [__dirname + '/../migrations/*{.ts,.js}']
+};
+
+
+export const AppDS = new DataSource(DataSourceConfig);
